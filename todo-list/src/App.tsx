a2d092ca84a4a2655d862152230141929459640e
WFriver <wrivera@quickapps.mx>
2024-11-04 15:33:36 -0600

    fist step
    fist step

---
+++ todo-list/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <div className="container py-4 px-3 mx-auto">
+    <h1>Hello, Bootstrap and Vite!</h1>
+    <button className="btn btn-success">Primary button</button>
+  </div>
+
+  )
+}
+
+export default App
